5d3c49ec426f40f08434d6acac6fb87de237a655
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-12-20 16:56:19 -0600

    Don't include the parts that don't need arguments

--- Universal/src/main/java/me/fixeddev/ebcm/util/UsageBuilder.java
+++ Universal/src/main/java/me/fixeddev/ebcm/util/UsageBuilder.java
@@ -1,14 +1,17 @@
 package me.fixeddev.ebcm.util;
 
 import me.fixeddev.ebcm.Command;
+import me.fixeddev.ebcm.part.ArgumentPart;
 import me.fixeddev.ebcm.part.LineConsumingPart;
 
 import java.util.stream.Collectors;
 
 public class UsageBuilder {
-    public static String getUsageForCommand(Command command, String label){
+
+    public static String getUsageForCommand(Command command, String label) {
         String usage = command.getParts().stream().filter(part -> part instanceof LineConsumingPart)
                 .map(part -> (LineConsumingPart) part)
+                .filter(part -> !(part instanceof ArgumentPart) || ((ArgumentPart) part).getConsumedArguments() > 0)
                 .map(part -> part.getLineRepresentation())
                 .collect(Collectors.joining(" "))
                 .replace("<command>", label);
