e199ec09d7fc1630922a0233649c41e729e0b16d
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-12-27 15:19:53 -0600

    Create a new CommandPart type for the values that are injected

---
+++ Universal/src/main/java/me/fixeddev/ebcm/part/InjectedValuePart.java
@@ -0,0 +1,58 @@
+package me.fixeddev.ebcm.part;
+
+import com.google.auto.value.AutoValue;
+import me.fixeddev.ebcm.util.ListAppender;
+
+import java.util.List;
+
+@AutoValue
+public abstract class InjectedValuePart implements CommandPart {
+    public abstract Class<?> getType();
+
+    public abstract String getInjectedName();
+
+    public static Builder builder(String name, Class<?> type) {
+        return new AutoValue_InjectedValuePart.Builder()
+                .setName(name)
+                .setInjectedName(name)
+                .setType(type)
+                .setRequired(true);
+    }
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+        private ListAppender<String> modifiersAppender = new ListAppender<>();
+
+        abstract Builder setName(String newName);
+
+        public abstract Builder setDescription(String newDescription);
+
+        public abstract Builder setRequired(boolean newRequired);
+
+        public abstract Builder setType(Class<?> type);
+
+        public abstract Builder setInjectedName(String newInjectedName);
+
+        public Builder setAllModifiers(List<String> modifiers){
+            this.modifiersAppender.set(modifiers);
+
+            return this;
+        }
+
+        public Builder addModifier(String modifier){
+            this.modifiersAppender.add(modifier);
+
+            return this;
+        }
+
+        abstract FlagPart.Builder setModifiers(List<String> modifiers);
+
+        abstract InjectedValuePart autoBuild();
+
+        public InjectedValuePart build() {
+            setModifiers(modifiersAppender.toList());
+
+            return autoBuild();
+        }
+    }
+}
