39dc770d70454fe698a0893a493bc3894cd41ad0
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-12-27 15:57:19 -0600

    Add directly on the Injected annotation if the part is required

--- Universal/src/main/java/me/fixeddev/ebcm/parametric/annotation/Injected.java
+++ Universal/src/main/java/me/fixeddev/ebcm/parametric/annotation/Injected.java
@@ -8,4 +8,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface Injected {
+    /**
+     * @return a boolean value that indicates if this injected value is required or not
+     */
+    boolean value() default false;
 }
