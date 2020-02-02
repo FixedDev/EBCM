1be2b3ebc6ef2a425668e83df5755e97c15b6ae6
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-02-02 15:04:19 -0600

    Register the CommandSenderProvide again in the BukkitModule

--- Bukkit/src/main/java/me/fixeddev/ebcm/bukkit/parameter/provider/BukkitModule.java
+++ Bukkit/src/main/java/me/fixeddev/ebcm/bukkit/parameter/provider/BukkitModule.java
@@ -3,12 +3,13 @@ package me.fixeddev.ebcm.bukkit.parameter.provider;
 import me.fixeddev.ebcm.parameter.provider.ParameterProviderRegistry;
 import me.fixeddev.ebcm.parameter.provider.ProvidersModule;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class BukkitModule implements ProvidersModule {
     @Override
     public void configure(ParameterProviderRegistry registry) {
-        //registry.registerParameterProvider(CommandSender.class, new CommandSenderProvider());
+        registry.registerParameterProvider(CommandSender.class, new CommandSenderProvider());
         registry.registerParameterProvider(OfflinePlayer.class, new OfflinePlayerProvider());
         registry.registerParameterProvider(Player.class, new PlayerProvider());
     }
