aec59fa7d5a5350a0ae24bcdc65da80a4a157311
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-03-22 14:45:08 -0600

    Why this wasn't committed yet?

---
+++ Bukkit/src/main/java/me/fixeddev/ebcm/bukkit/BukkitMessager.java
@@ -0,0 +1,15 @@
+package me.fixeddev.ebcm.bukkit;
+
+import me.fixeddev.ebcm.Messager;
+import me.fixeddev.ebcm.NamespaceAccesor;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class BukkitMessager implements Messager {
+    @Override
+    public void sendMessage(NamespaceAccesor namespace, String message) {
+        CommandSender sender = namespace.getObject(CommandSender.class, BukkitCommandManager.SENDER_NAMESPACE);
+
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+}
---
+++ Bungee/src/main/java/me/fixeddev/ebcm/bungee/BungeeMessager.java
@@ -0,0 +1,17 @@
+package me.fixeddev.ebcm.bungee;
+
+import me.fixeddev.ebcm.Messager;
+import me.fixeddev.ebcm.NamespaceAccesor;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class BungeeMessager implements Messager {
+    @Override
+    public void sendMessage(NamespaceAccesor namespace, String message) {
+        CommandSender sender = namespace.getObject(CommandSender.class, BungeeCommandManager.SENDER_NAMESPACE);
+
+        message = ChatColor.translateAlternateColorCodes('&',message);
+        sender.sendMessage(TextComponent.fromLegacyText(message));
+    }
+}
