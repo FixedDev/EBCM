6ae86a341f08d9d9666e71c05a3c10e4ac7a5308
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-05-21 16:49:17 -0500

    Don't use the backing list, iterate the stack

--- Universal/src/main/java/me/fixeddev/ebcm/parameter/provider/defaults/StringProvider.java
+++ Universal/src/main/java/me/fixeddev/ebcm/parameter/provider/defaults/StringProvider.java
@@ -1,11 +1,13 @@
 package me.fixeddev.ebcm.parameter.provider.defaults;
 
 import me.fixeddev.ebcm.NamespaceAccesor;
+import me.fixeddev.ebcm.exception.NoMoreArgumentsException;
 import me.fixeddev.ebcm.parameter.provider.ParameterProvider;
 import me.fixeddev.ebcm.part.CommandPart;
 import me.fixeddev.ebcm.stack.StackSlice;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 /*
 Ok, if you're reading this, you may ask why this class is not a SingleArgumentProvider
@@ -15,7 +17,13 @@ can be joined to form a single string in this form "arg arg2 arg3"
 public class StringProvider implements ParameterProvider<String> {
 
     @Override
-    public Result<String> transform(StackSlice arguments, NamespaceAccesor namespaceAccesor, CommandPart part) {
-        return Result.createResult(String.join(" ", arguments.getBacking()));
+    public Result<String> transform(StackSlice arguments, NamespaceAccesor namespaceAccesor, CommandPart part) throws NoMoreArgumentsException {
+        StringJoiner joiner = new StringJoiner(" ");
+
+        while (arguments.hasNext()){
+            joiner.add(arguments.next());
+        }
+
+        return Result.createResult(joiner.toString());
     }
 }
