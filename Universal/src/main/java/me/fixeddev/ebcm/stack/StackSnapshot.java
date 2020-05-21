0739e674d81fba0a4217e98264b290b8764bd9ed
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-05-21 15:13:14 -0500

    Remove the StackSnapshot interface and rename the BasicStackSnapshot to StackSnapshot

--- Universal/src/main/java/me/fixeddev/ebcm/stack/StackSnapshot.java
+++ Universal/src/main/java/me/fixeddev/ebcm/stack/StackSnapshot.java
@@ -3,30 +3,121 @@ package me.fixeddev.ebcm.stack;
 import me.fixeddev.ebcm.exception.CommandParseException;
 import me.fixeddev.ebcm.exception.NoMoreArgumentsException;
 
-public interface StackSnapshot {
-    boolean hasNext();
+import java.util.ArrayList;
+import java.util.List;
 
-    String next() throws NoMoreArgumentsException;
+public class StackSnapshot {
 
-    String peek() throws NoMoreArgumentsException;
+    List<String> backing;
+    int position;
 
-    String current();
+    public StackSnapshot(ArgumentStack stack, int position) {
+        this.backing = new ArrayList<>(stack.getBacking());
+        this.position = position;
+    }
 
-    String remove();
+    public boolean hasNext() {
+        return (backing.size() - 1) > position;
+    }
 
-    int getPosition();
+    public String next() throws NoMoreArgumentsException {
+        if (!hasNext()) {
+            throw new NoMoreArgumentsException(backing.size(), position);
+        }
 
-    int getSize();
+        position++;
 
-    int nextInt() throws CommandParseException;
+        return backing.get(position);
+    }
 
-    float nextFloat() throws CommandParseException;
+    public String peek() throws NoMoreArgumentsException {
+        int nextPosition = position + 1;
 
-    double nextDouble() throws CommandParseException;
+        if (!hasNext()) {
+            throw new NoMoreArgumentsException(backing.size(), position);
+        }
 
-    byte nextByte() throws CommandParseException;
+        return backing.get(nextPosition);
+    }
 
-    boolean nextBoolean() throws CommandParseException;
+    public String current() {
+        if (position == -1) {
+            throw new IllegalStateException("You must advance the stack at least once before using the current() method!");
+        }
 
-    void markAsConsumed();
+        return backing.get(position);
+    }
+
+    public String remove() {
+        if (position == -1) {
+            throw new IllegalStateException("You must advance the stack at least once before using the remove() method!");
+        }
+
+        String toRemove = current();
+        backing.remove(current());
+
+        return toRemove;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+
+    public int getSize() {
+        return backing.size();
+    }
+
+    public int nextInt() throws CommandParseException {
+        String next = next();
+        try {
+            return Integer.parseInt(next);
+        } catch (NumberFormatException e) {
+            throw new CommandParseException("Failed to parse the string " + next + " as int!");
+        }
+    }
+
+    public float nextFloat() throws CommandParseException {
+        String next = next();
+
+        try {
+            return Float.parseFloat(next);
+        } catch (NumberFormatException e) {
+            throw new CommandParseException("Failed to parse the string " + next + " as float!");
+        }
+    }
+
+    public double nextDouble() throws CommandParseException {
+        String next = next();
+
+        try {
+            return Double.parseDouble(next);
+        } catch (NumberFormatException e) {
+            throw new CommandParseException("Failed to parse the string " + next + " as double!");
+        }
+    }
+
+    public byte nextByte() throws CommandParseException {
+        String next = next();
+
+        try {
+            return Byte.parseByte(next);
+        } catch (NumberFormatException e) {
+            throw new CommandParseException("Failed to parse the string " + next + " as byte!");
+        }
+    }
+
+    public boolean nextBoolean() throws CommandParseException {
+        String next = next();
+
+        if (!next.equalsIgnoreCase("true") && !next.equalsIgnoreCase("false")) {
+            throw new CommandParseException("Failed to parse the string " + next + " as boolean!");
+        }
+
+        return Boolean.parseBoolean(next);
+    }
+
+    public void markAsConsumed() {
+        this.position = backing.size();
+    }
 }
