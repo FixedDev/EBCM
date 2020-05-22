d0b26f71b1808de9b055f6b5b243c9cca39d892a
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-05-21 20:23:41 -0500

    Change names

--- Universal/src/main/java/me/fixeddev/ebcm/parametric/annotation/Optional.java
+++ Universal/src/main/java/me/fixeddev/ebcm/parametric/annotation/Optional.java
@@ -17,6 +17,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-public @interface Default {
+public @interface Optional {
     String[] value() default {};
 }
