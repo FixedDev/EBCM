55a583c16dbebdbfe8ca424dda3f2ef88dba474b
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-06-04 23:17:43 -0500

    Remove the messageId from the messager since isn't needed anymore

--- Universal/src/main/java/me/fixeddev/ebcm/Messager.java
+++ Universal/src/main/java/me/fixeddev/ebcm/Messager.java
@@ -1,9 +1,5 @@
 package me.fixeddev.ebcm;
 
 public interface Messager {
-    default void sendMessage(NamespaceAccesor namespaceAccesor, String message) {
-        sendMessage(namespaceAccesor, "UNKNOWN", message);
-    }
-
-    void sendMessage(NamespaceAccesor namespaceAccesor, String messageId, String message, String... parameters);
+    void sendMessage(NamespaceAccesor namespaceAccesor, String message, Object... parameters);
 }
